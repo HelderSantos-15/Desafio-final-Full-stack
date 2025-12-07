815203d22c657b329090b10a4bc988164dc78c24
heldersantos <heldermaximo.15@gmail.com>
2025-12-07 20:56:59 -0300

    Configurando o App.js

--- cineflix-front-end/src/App.jsx
+++ cineflix-front-end/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -7,68 +13,29 @@ import DetailsLocal from "./pages/DetailsLocal";
 import Favoritos from "./pages/Favoritos";
 import Header from "./components/Header";
 
-// Componente para rotas privadas
-function PrivateRoute({ children }) {
-  const isLoggedIn = !!localStorage.getItem("token"); // ajuste se você usa JWT ou flag de login
-  return isLoggedIn ? children : <Navigate to="/login" replace />;
-}
-
 function AppContent() {
   const location = useLocation();
 
   // TELAS ONDE NÃO MOSTRA O HEADER
-  const hideHeader = location.pathname === "/login" || location.pathname === "/register";
+  const hideHeader =
+    location.pathname === "/login" || location.pathname === "/register";
 
   return (
     <>
       {!hideHeader && <Header />}
 
       <Routes>
-        {/* 🔥 ROTA RAIZ */}
-        <Route
-          path="/"
-          element={
-            localStorage.getItem("token") ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />
-          }
-        />
+        {/* 🔥 ROTA RAIZ AGORA REDIRECIONA PRO LOGIN */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
-        {/* Rotas públicas */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* Rotas privadas */}
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/details/:id"
-          element={
-            <PrivateRoute>
-              <Details />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/details-local/:id"
-          element={
-            <PrivateRoute>
-              <DetailsLocal />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/favoritos"
-          element={
-            <PrivateRoute>
-              <Favoritos />
-            </PrivateRoute>
-          }
-        />
+        {/* Rotas protegidas depois */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/details-local/:id" element={<DetailsLocal />} />
+        <Route path="/favoritos" element={<Favoritos />} />
       </Routes>
     </>
   );
